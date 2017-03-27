41d9eeb7cd8ba5662a33bf1acd4563338dee4a4c
Jordi Valls <jvb35@alu.ua.es>
2017-03-27 21:24:27 +0200

    Listado de peliculas realizado

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -3,10 +3,23 @@
 @section('content')
 
 <h1>Cartelera</h1>
-<ul>
-    @foreach ($films as $film)
-        <li><a href="films/{{$film->id}}">{{$film->name}}</a></li>
-    @endforeach
-</ul>
+
+
+
+<div class="row">
+  <div class="col-sm-12">
+  @foreach ($films as $film)
+    <a href="/films/{{$film->id}}" class="text-center" >
+      <img style="width :150px; width: 150px;
+    height: 220px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;" src="/images/{{$film->name}}.jpg" >
+    </a>
+  @endforeach
+  </div>
+</div>
+
 
 @endsection
