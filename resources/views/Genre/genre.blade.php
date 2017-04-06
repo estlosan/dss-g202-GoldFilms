09182009f77be83a42cb004009ea4bb2d1b2be5b
Mr.Robot <robot@Mr.Robot>
2017-04-06 19:50:23 +0200

    Mejora vistas de las peliculas en genero

--- resources/views/Genre/genre.blade.php
+++ resources/views/Genre/genre.blade.php
@@ -11,8 +11,14 @@
     <h3>Peliculas del genero:</h3>
     
     @foreach ($genre->film as $film)
-        <a href="/films/{{$film->id}}" class="text-center" >
-        <img class="image_films" src="/images/{{$film->name}}.jpg" >
+    <div class="films_row" style="width:200px; margin:auto; display:inline-block; vertical-align: middle;">
+        <div class="thumbnail" style="width:150px; height:278px;">
+            <a href="/films/{{$film->id}}"><img src="/images/{{$film->name}}.jpg" style="width:150px; height:200px;"></a>
+                <div class="caption" style="text-align:center;">
+                    <p><a href="/films/{{$film->id}}"> {{$film->name}}</a></p>
+                </div>
+         </div>
+    </div>
     @endforeach
 </div>  
 
