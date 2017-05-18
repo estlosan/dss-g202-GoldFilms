837044b220749679fe07e3708996e61acf9b0fe6
Oscar <olpp1@alu.ua.es>
2017-05-18 20:15:49 +0200

    añadir actor COMPLETO

--- resources/views/Actor/create_actor.blade.php
+++ resources/views/Actor/create_actor.blade.php
@@ -134,6 +134,12 @@ Añade una imagen</div>
                 </ul>
             </ul>
         </div>
+         @if ($errors->has('films'))
+    <div class="alert alert-danger" role="alert">
+    <span class="glyphicon glyphicon-exclamation-sign"  aria-hidden="true" ></span>
+    <span class="sr-only">Error</span>
+Añade al menos una película</div>
+@endif
       </div>
     </div>
 
